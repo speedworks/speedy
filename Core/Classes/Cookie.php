4e4060e27670a8a4c4df551a1f90ecac411dc49d
shakti.phartiyal@technoxom.com <shakti.phartiyal@technoxom.com>
2017-01-23 11:22:51 +0530

    :sparkles: :ship: Cookie Wrapper

--- Core/Classes/Cookie.php
+++ Core/Classes/Cookie.php
@@ -13,16 +13,73 @@ class Cookie
     {
 
     }
-    public static function set()
-    {
 
+    /**
+     * Set a Cookie
+     * @param String $cookieName
+     * @param String $cookieValue [optional]
+     * @param int $expiryDays [optional]
+     * @param String $path [optional]
+     * @param String $domain [optional]
+     * @param bool $secure [optional]
+     * @param bool $httpOnly [optional]
+     * @return bool
+     */
+    public static function set($cookieName, $cookieValue=null, $expiryDays=365, $path=null, $domain=null, $secure=null, $httpOnly=null)
+    {
+        return setcookie($cookieName, $cookieValue, time()+(86400 * $expiryDays), $path, $domain, $secure, $httpOnly);
     }
-    public static function get()
-    {
 
+    /**
+     * Get value of a cookie or get all cookies
+     * @param null String $cookieName [optional]
+     * @return String/array if exists | null if cookie does not exist
+     */
+    public static function get($cookieName = null)
+    {
+        if($cookieName == null)
+        {
+            return $_COOKIE;
+        }
+        if(isset($_COOKIE[$cookieName]))
+        {
+            return $_COOKIE[$cookieName];
+        }
+        return null;
     }
-    public static function has()
-    {
 
+    /**
+     * Checks if cookie exists or not
+     * @param $cookieName
+     * @return bool
+     */
+    public static function has($cookieName)
+    {
+        return isset($_COOKIE[$cookieName]);
+    }
+
+    /**
+     * Delete a cookie or all cookies
+     * @param String $cookieName [optional]
+     * @return bool
+     */
+    public static function delete($cookieName = null)
+    {
+        if($cookieName == null)
+        {
+            if (isset($_SERVER['HTTP_COOKIE']))
+            {
+                $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+                foreach($cookies as $cookie)
+                {
+                    $parts = explode('=', $cookie);
+                    $name = trim($parts[0]);
+                    setcookie($name, '', time()-1000);
+                    setcookie($name, '', time()-1000, '/');
+                }
+            }
+            return true;
+        }
+        return setcookie($cookieName, "", time() - 1000);
     }
 }
