692dab063cb21fc7485aa285ca596d7af3af1bd6
shakti.phartiyal@technoxom.com <shakti.phartiyal@technoxom.com>
2017-01-27 18:45:08 +0530

    :construction: Mailer

---
+++ Core/Classes/Mailer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @Author: Shakti Phartiyal
+ * Date: 01/27/17
+ * Time: 06:43 PM
+ */
+namespace Core\Mailer;
+
+class Mailer
+{
+    public function __construct()
+    {
+
+    }
+}
